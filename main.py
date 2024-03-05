5e3a58de2708214ce01e994ada38071372cd2574
GreensCoding <162338406+GreensCoding@users.noreply.github.com>
2024-03-05 06:52:10 -0800

    Created main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# DO NOT REMOVE THIS IMPORT FOR THIS TO WORK.
+import Gython as console
+# CODE HERE
+GPlus = console.GyPlus
+console.Say(f"{console.GREEN}Hi! Welcome to Gython! Fork this repl to get started!{console.RESET}")
+console.Say(f"Your Current Version is: {console.Version}")
+GPlus.Warn(f"Please dont try to copy!", level=4)
+# type Console.(commands here) to view and type all the commands!  

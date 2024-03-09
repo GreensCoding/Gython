c8e03854d489e947a1797467e285a68d8476f2ef
Green's Coding <162338406+GreensCoding@users.noreply.github.com>
2024-03-08 23:57:12 -0800

    Updated Gython.py

--- Gython.py
+++ Gython.py
@@ -1,32 +1,71 @@
 ### this file is very important for Gython to work. Please do not remove ###
-# also I made it ez to use and ez to make so you can use it in your projects
+# also I made it ez to use to make so you can use it in your projects
 
 import sys
 import os
 import time
-from colorama import Fore, Back, Style 
-
+from typing_extensions import *
+from typing_extensions import Self
+from colorama import Fore as __Fore__
+from colorama import Style as __Style__
+from colorama import Back as __Back__
+import webbrowser as __webbrowser__
+import requests as __requests__
+from bs4 import BeautifulSoup as __BeautifulSoup__
 # Version
-Version = 2.2
+Version = "2.3.6"
 # Colors
-GREEN = f"{Fore.GREEN}"
-RESET = f"{Style.RESET_ALL}"
-RESETCOLOR = f"{Fore.RESET}"
-RED  = f"{Fore.RED}"
-BLUE = f"{Fore.BLUE}"
-YELLOW = f"{Fore.YELLOW}"
-CYAN = f"{Fore.CYAN}"
-MAGENTA = f"{Fore.MAGENTA}"
-WHITE = f"{Fore.WHITE}"
-BLACK = f"{Fore.BLACK}"
-LIGHTBLACK = f"{Fore.LIGHTBLACK_EX}"
-LIGHTBLUE = f"{Fore.LIGHTBLUE_EX}"
-LIGHTCYAN = f"{Fore.LIGHTCYAN_EX}"
-LIGHTGREEN = f"{Fore.LIGHTGREEN_EX}"
-LIGHTMAGENTA = f"{Fore.LIGHTMAGENTA_EX}"
-LIGHTRED = f"{Fore.LIGHTRED_EX}"
-LIGHTWHITE = f"{Fore.LIGHTWHITE_EX}"
-LIGHTYELLOW = f"{Fore.LIGHTYELLOW_EX}"
+class ColorPanel:
+  GREEN = f"{__Fore__.GREEN}"
+  RESET = f"{__Style__.RESET_ALL}"
+  RESETCOLOR = f"{__Fore__.RESET}"
+  RED  = f"{__Fore__.RED}"
+  BLUE = f"{__Fore__.BLUE}"
+  YELLOW = f"{__Fore__.YELLOW}"
+  CYAN = f"{__Fore__.CYAN}"
+  MAGENTA = f"{__Fore__.MAGENTA}"
+  WHITE = f"{__Fore__.WHITE}"
+  BLACK = f"{__Fore__.BLACK}"
+  LIGHTBLACK = f"{__Fore__.LIGHTBLACK_EX}"
+  LIGHTBLUE = f"{__Fore__.LIGHTBLUE_EX}"
+  LIGHTCYAN = f"{__Fore__.LIGHTCYAN_EX}"
+  LIGHTGREEN = f"{__Fore__.LIGHTGREEN_EX}"
+  LIGHTMAGENTA = f"{__Fore__.LIGHTMAGENTA_EX}"
+  LIGHTRED = f"{__Fore__.LIGHTRED_EX}"
+  LIGHTWHITE = f"{__Fore__.LIGHTWHITE_EX}"
+  LIGHTYELLOW = f"{__Fore__.LIGHTYELLOW_EX}"
+  class BackgroundColors:
+    LIGHTGREEN = f"{__Back__.LIGHTGREEN_EX}"
+    BLACK = f"{__Back__.BLACK}"
+    RESET = f"{__Back__.RESET}"
+    GREEN = f"{__Back__.GREEN}"
+    BLUE = f"{__Back__.BLUE}"
+    RED = f"{__Back__.RED}"
+    YELLOW = f"{__Back__.YELLOW}"
+    CYAN = f"{__Back__.CYAN}"
+    MAGENTA = f"{__Back__.MAGENTA}"
+    WHITE = f"{__Back__.WHITE}"
+    LIGHTBLACK = f"{__Back__.LIGHTBLACK_EX}"
+    LIGHTBLUE = f"{__Back__.LIGHTBLUE_EX}"
+    LIGHTCYAN = f"{__Back__.LIGHTCYAN_EX}"
+    LIGHTGREEN = f"{__Back__.LIGHTGREEN_EX}"
+    LIGHTMAGENTA = f"{__Back__.LIGHTMAGENTA_EX}"
+    LIGHTRED = f"{__Back__.LIGHTRED_EX}"
+    LIGHTWHITE = f"{__Back__.LIGHTWHITE_EX}"
+    LIGHTYELLOW = f"{__Back__.LIGHTYELLOW_EX}"
+class TextEditPanel:
+  ITALIC = "\x1B[3m"
+  BOLD = "\x1B[1m"
+  ITALIC_END = "\x1B[0m"
+  BOLD_END = "\x1B[0m"
+  UNDERLINE = "\x1B[4m"
+  UNDERLINE_END = "\x1B[0m"
+  
+class GyWebPackages:
+  import os
+  import sys
+  import webbrowser as wb
+
 Credits = f"""-----------------------------------------------
 Green's Coding: 
 https://replit.com/@GreenGaming6 (Creator/Publisher)
@@ -37,48 +76,48 @@ This is an open-source repl made on REPLIT.com, Please do not edit or do anythin
 If you make a project using this please do credits [if you dont want it's ok], Thanks for reading."""
 # Error Codes
 __ERRORCODE001 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 (PrintDone) Value must be a Boolean Value 'True' or 'False', Please Try Again.
 ErrorCode: 001
 """
 __ERRORCODE002 = f"""
-    {Fore.RED}(CodeError):
+    {__Fore__.RED}(CodeError):
     Value must be an Integer Value, Please Try Again.
     ErrorCode: 002
     """
 __ERRORCODE003 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Value must be a String Value, Please Try Again.
 ErrorCode: 003
 """
 
 __ERRORCODE004 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Value must be a List Value, Please Try Again.
 ErrorCode: 004
 """
 
 
 __ERRORCODE005 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Invalid Value, Please Try Again.
 ErrorCode: 005
 """
 
 __ERRORCODE006 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Unreachable or No Value, Please Try Again.
 ErrorCode: 006
 """
 
 __ERRORCODE007 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Value/File does not exist, Please Try Again.
 ErrorCode: 007
 """
 
 __ERRORCODE008 = f"""
-{Fore.RED}(CodeError):
+{__Fore__.RED}(CodeError):
 Value does not exist, Use 1-5, Please Try Again.
 ErrorCode: 008
 """
@@ -99,15 +138,15 @@ class GyPlus:
       Say(__ERRORCODE006)
   def Warn(warn, level):
     if level == 1 or level == "1":
-      Say(f"{LIGHTRED}{warn}{RESETCOLOR}")
+      Say(f"{ColorPanel.LIGHTRED}{warn}{ColorPanel.RESETCOLOR}")
     elif level == 2 or level == "2":
-      Say(f"{LIGHTYELLOW}{warn}{LIGHTYELLOW}")
+      Say(f"{ColorPanel.LIGHTYELLOW}{warn}{ColorPanel.LIGHTYELLOW}")
     elif level == 3 or level == "3":
-      Say(f"{YELLOW}{warn}{RESETCOLOR}")
+      Say(f"{ColorPanel.YELLOW}{warn}{ColorPanel.RESETCOLOR}")
     elif level == 4 or level == "4":
-      Say(f"{RED}{warn}{RESETCOLOR}")
+      Say(f"{ColorPanel.RED}{warn}{ColorPanel.RESETCOLOR}")
     elif level == 5 or level == "5":
-      Say(f"{MAGENTA}{warn}{RESETCOLOR}")
+      Say(f"{ColorPanel.MAGENTA}{warn}{ColorPanel.RESETCOLOR}")
     elif level:
       Say(__ERRORCODE008)
 ######### Functions #########
@@ -124,6 +163,8 @@ def Maths(num1, num2, operator):
     return num1 * num2
   elif operator == "/":
     return num1 / num2
+  elif operator == "^":
+    return num1 ^ num2
 # Basically the print() function
 def Say(string):
   string = str(string)
@@ -238,3 +279,42 @@ def readfile(file):
   with open(file, "r") as f:
     contents = f.read()
     Say(f"The contents of the file {file} are:\n{contents}")
+# writes a file
+def writefile(file, text):
+    file = str(file)
+    with open(file, "w") as f:
+        f.write(text)
+    Say(f"The file {file} has been written with the text {text} ")
+# removes a file
+def removedir(directory):
+  os.rmdir(directory)
+  Say("Removed Directory Successfully")
+# pops a value from a list
+def pop(list, index):
+  list = list
+  index = int(index)
+  list.pop(index)
+  Say(f"Popped {index} from {list}")
+# adds a value to a list
+def addvalue(list, value):
+  list = list
+  value = value
+  list.append(value)
+# removes a value from a list
+def removevalue(list, value):
+  list=list
+  value = value
+  list.remove(value)
+# gets the length
+def GetLength(value):
+  value = value
+  length = len(value)
+# web scrapes a selected website\
+def webscrape(url):
+  response = __requests__.get(url)
+  html_content = response.text
+  soup = __BeautifulSoup__(html_content, "html.parser")
+  return soup.prettify()
+def fork(url):
+  Say("Forking...")
+  __webbrowser__.open(url)

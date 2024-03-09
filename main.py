234c59a2e63f5d558029326c57399f304e79cbce
Green's Coding <162338406+GreensCoding@users.noreply.github.com>
2024-03-08 23:57:56 -0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # DO NOT REMOVE THIS IMPORT FOR THIS TO WORK.
 import Gython as console
 # CODE HERE
+
+GWeb = console.GyWebPackages
 GPlus = console.GyPlus
-console.Say(f"{console.GREEN}Hi! Welcome to Gython! Fork this repl to get started!{console.RESET}")
+GColors = console.ColorPanel
+GText = console.TextEditPanel
+GBackground = GColors.BackgroundColors
+
+console.Say(f"{GColors.GREEN}Hi! Welcome to Gython! Fork this repl to get started!{GColors.RESET}")
 console.Say(f"Your Current Version is: {console.Version}")
-GPlus.Warn(f"Please dont try to copy!", level=4)
+GPlus.Warn(f"You can read the license in [https://github.com/GreensCoding/Gython/blob/main/LICENSE]", level=4)
+# ignore the "str" is incompatible with "GPlus"
 # type Console.(commands here) to view and type all the commands!  
